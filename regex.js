9e7460f001068421bcd4e73267afbc147f4c888c
jwatts41 <wattsjonovan@yahoo.com>
2022-08-15 19:57:24 -0400

    added in email

--- regex.js
+++ regex.js
@@ -53,7 +53,10 @@ Expected Output:
 
 */ 
 
-let emailReg = /write your regex here/
+let emailReg = /\w+@[a-zA-Z]+.[a-zA-Z]+/
+let emailArr = sampleApp.match(emailReg)
+console.log(emailArr)
+
 
 /* 
 
